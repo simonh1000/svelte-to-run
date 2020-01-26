5ee6f16c98ec0a5ffbc47bc10515bcead671ad40
Simon Hampton <hotbelgo@gmail.com>
2020-01-26 08:27:29 +0100

    add pwa support

--- svelte/src/main.js
+++ svelte/src/main.js
@@ -1,3 +1,11 @@
+// Check that service workers are supported
+if ("serviceWorker" in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js");
+    });
+}
+
 import "./App.scss";
 import App from "./App.svelte";
 
