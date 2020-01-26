feae22eb2c9b2b2f6992972b60153799c4520e85
Simon Hampton <hotbelgo@gmail.com>
2020-01-26 08:17:17 +0100

    hot reloading

--- svelte/webpack.config.js
+++ svelte/webpack.config.js
@@ -3,58 +3,73 @@ const path = require("path");
 
 const mode = process.env.NODE_ENV || "development";
 const prod = mode === "production";
+const dev = !prod;
 
 module.exports = {
-  entry: {
-    bundle: ["./src/main.js"]
-  },
-  resolve: {
-    alias: {
-      svelte: path.resolve("node_modules", "svelte")
+    entry: {
+        bundle: ["./src/main.js"]
     },
-    extensions: [".mjs", ".js", ".svelte", ".scss"],
-    mainFields: ["svelte", "browser", "module", "main"]
-  },
-  output: {
-    path: __dirname + "/public",
-    filename: "[name].js",
-    chunkFilename: "[name].[id].js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.svelte$/,
-        use: {
-          loader: "svelte-loader",
-          options: {
-            emitCss: true,
-            hotReload: true
-          }
-        }
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              sassOptions: {
-                includePaths: ["./src/styles", "./node_modules"]
-              }
+    resolve: {
+        alias: {
+            svelte: path.resolve("node_modules", "svelte")
+        },
+        extensions: [".mjs", ".js", ".svelte", ".scss"],
+        mainFields: ["svelte", "browser", "module", "main"]
+    },
+    output: {
+        path: __dirname + "/public",
+        filename: "[name].js",
+        chunkFilename: "[name].[id].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svelte$/,
+                use: {
+                    loader: "svelte-loader-hot",
+                    options: {
+                        dev,
+                        hotReload: true,
+                        hotOptions: {
+                            // whether to preserve local state (i.e. any `let` variable) or
+                            // only public props (i.e. `export let ...`)
+                            noPreserveState: false,
+                            // optimistic will try to recover from runtime errors happening
+                            // during component init. This goes funky when your components are
+                            // not pure enough.
+                            optimistic: true
+                        }
+                    }
+                }
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sassOptions: {
+                                includePaths: ["./src/styles", "./node_modules"]
+                            }
+                        }
+                    }
+                ]
             }
-          }
         ]
-      }
-    ]
-  },
-  mode,
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
-    })
-  ],
-  devtool: prod ? false : "source-map"
+    },
+    mode,
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ],
+    devtool: prod ? false : "source-map",
+    devServer: {
+        contentBase: "public",
+        hot: true,
+        overlay: true
+    }
 };
