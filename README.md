d65f384e9e34c542a198a2453f0b9bed0d62dae6
Simon Hampton <hotbelgo@gmail.com>
2020-02-15 18:53:19 +0100

    use enableHighAccuracy

--- README.md
+++ README.md
@@ -25,3 +25,4 @@ The app records location, but all the data is saved to your brower's local stora
 ## Changelog
 
 1.1 - add warning about not turning off phone
+1.1.1 - use enableHighAccuracy for geolocation
--- src/js/geolocation.js
+++ src/js/geolocation.js
@@ -2,6 +2,9 @@ var watchId;
 
 export const startGeolocation = function(cb) {
     console.log("Starting geolocation");
+    const options = {
+        enableHighAccuracy: true
+    };
     watchId = navigator.geolocation.watchPosition(
         pos => {
             cb({
@@ -21,7 +24,8 @@ export const startGeolocation = function(cb) {
                 tag: "error",
                 payload: JSON.stringify(err, Object.getOwnPropertyNames(err))
             });
-        }
+        },
+        options
     );
 };
 
