b4f0164a6c7e731df84300c7933e20c448ce17e8
Simon Hampton <hotbelgo@gmail.com>
2020-02-22 09:09:02 +0100

    less debugging

--- src/js/dayRuns.js
+++ src/js/dayRuns.js
@@ -75,7 +75,7 @@ export function summariseUpto(list, elapsed) {
 // gets last completed run from history
 // TODO move to persistence?
 export function getLastRun(history) {
-    console.log("getLastRun", history);
+    // console.log("getLastRun", history);
     let tmp = history.reduce((acc, run) => {
         // if we already have the latest run then keep returning that
         if (acc > -1) return acc;
@@ -83,7 +83,7 @@ export function getLastRun(history) {
         if (run.completed && typeof run.title === "number") return run.title;
         return acc;
     }, -1);
-    console.log("last run ", tmp);
+    // console.log("last run ", tmp);
     return tmp;
 }
 
